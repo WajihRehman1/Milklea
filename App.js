5e746e190bbe8f561efa8b9d209d4c012f21a0af
WajihRehman1 <mwajihrehman@gmail.com>
2022-05-10 23:59:12 +0500

    wajih

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TextInput,
+  Button,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.Logo} source={require("./assets/Logo.png")} />
+      <View>
+        <TextInput style={styles.input} value={Text} placeholder="Username" />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={true}
+        />
+      </View>
+      <Text style={{ color: "rgb(0, 46, 107)" }}>
+        Forgot Passowrd | Register Now {"\n"}
+      </Text>
+      <View>
+        <Button title="Login" color="rgb(0, 46, 107)"></Button>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Logo: {
+    width: "60%",
+    height: "30%",
+    resizeMode: "stretch",
+    alignItems: "flex-start",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "rgb(0, 46, 107)",
+    padding: 10,
+    width: 300,
   },
 });
